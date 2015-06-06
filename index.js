d03ed32b3dafaf4b02d7d2b882921eb9fcc71315
jumbo ken <subsubmailer@gmail.com>
2015-06-06 15:23:57 +0900

    close time span

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ console.log("websocket server created")
 
 wss.on("connection", function(ws) {
   
-  var id = setInterval(function() {
-    ws.send(JSON.stringify(new Date()), function() {  })
-  }, 1000)
+  // var id = setInterval(function() {
+    // ws.send(JSON.stringify(new Date()), function() {  })
+  // }, 1000)
 
   console.log("websocket connection open")
 
